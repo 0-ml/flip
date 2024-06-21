14438914fb288e38d9304f1b3acbb6d8f8c6408b
ldpbuaa <ldpbuaa@163.com>
2024-06-22 01:06:09 +0800

    update help info

--- flcore/models/__init__.py
+++ flcore/models/__init__.py
@@ -1,9 +1,1 @@
 from flcore.models.cnn.fedavg_cnn import FedAvgCNN
-
-
-
-
-def get_model(args, ):
-    if args.model == 'CNN':
-        model = FedAvgCNN(in_features=1, num_classes=args.num_classes, dim=1024).to(args.device)
-    return model
